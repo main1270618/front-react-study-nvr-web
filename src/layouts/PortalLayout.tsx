dec9c2072c5cf799e032068b04b2dd4a5f65536b
wontae kim <main1270618@gmail.com>
2023-01-10 23:56:21 +0900

    PotalLayout  nav-list 소스 수정

--- src/layouts/PortalLayout.tsx
+++ src/layouts/PortalLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
 
 import '../assets/css/main.css';
 
@@ -10,7 +10,7 @@ function PortalLayout () {
                 <header>
                     <div id='search'>
                         <div style={{paddingRight: '25px'}}>
-                            <img src={ require('../assets/img/logo.png') } style={{width: '222px', height: '52px'}} />
+                            <img src={ require('../assets/img/logo.png') } style={{width: '222px', height: '52px'}} alt = "face" />
                         </div>
                         <div style={{ display: 'flex', alignItems: 'center' }}>
                             <input id='search_input'/>
@@ -20,58 +20,58 @@ function PortalLayout () {
                     <div id='gnb'>
                         <ul id='nav-list'>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>메일</a>
+                                <a href='/#'>메일</a>
                             </li>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>카페</a>
+                                <a href='/#'>카페</a>
                             </li>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>블로그</a>
+                                <a href='/#'>블로그</a>
                             </li>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>지식iN</a>
+                                <a href='/#'>지식iN</a>
                             </li>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>쇼핑</a>
+                                <a href='/#'>쇼핑</a>
                             </li>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>쇼핑</a>
+                                <a href='/#'>쇼핑</a>
                             </li>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>쇼핑LIVE</a>
+                                <a href='/#'>쇼핑LIVE</a>
                             </li>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>Pay</a>
+                                <a href='/#'>Pay</a>
                             </li>
                             <li style={{ color: "#19ce60" }}>
-                                <a href='#'>TV</a>
+                                <a href='/#'>TV</a>
                             </li>
                             <li style={{ color: "#000000" }}>
-                                <a href='#'>사전</a>
+                                <a href='/#'>사전</a>
                             </li>
                             <li style={{ color: "#000000" }}>
-                                <a href='#'>뉴스</a>
+                                <a href='/#'>뉴스</a>
                             </li>
                             <li style={{ color: "#000000" }}>
-                                <a href='#'>증권</a>
+                                <a href='/#'>증권</a>
                             </li>
                             <li style={{ color: "#000000" }}>
-                                <a href='#'>부동산</a>
+                                <a href='/#'>부동산</a>
                             </li>
                             <li style={{ color: "#000000" }}>
-                                <a href='#'>지도</a>
+                                <a href='/#'>지도</a>
                             </li>
                             <li style={{ color: "#000000" }}>
-                                <a href='#'>VIBE</a>
+                                <a href='/#'>VIBE</a>
                             </li>
                             <li style={{ color: "#000000" }}>
-                                <a href='#'>도서</a>
+                                <a href='/#'>도서</a>
                             </li>
                             <li style={{ color: "#000000" }}>
-                                <a href='#'>웹툰</a>
+                                <a href='/#'>웹툰</a>
                             </li>
                         </ul>
-                        <a id='more_btn' href='#'>더보기</a>
+                        <a id='more_btn' href='/#'>더보기</a>
                     </div>  
                 </header>
                 <main>
